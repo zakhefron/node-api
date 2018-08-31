682a457a3d399c63f0f3dba109b96052903880d9
Zeeshan Ahmad <ziishaned@gmail.com>
2018-08-31 14:14:12 +0400

    Add users endpoints

---
+++ src/controllers/user.js
@@ -0,0 +1,75 @@
+export class UserController {
+  /**
+   * Get user from jwt.
+   *
+   * @param {object} req
+   * @param {object} res
+   */
+  me(req, res) {
+  }
+
+  /**
+   * Login user by generating a new jwt token.
+   *
+   * @param {object} req
+   * @param {object} res
+   */
+  login(req, res) {
+  }
+
+  /**
+   * Register and login user by generating a new jwt token.
+   *
+   * @param {object} req
+   * @param {object} res
+   */
+  register(req, res) {
+  }
+
+  /**
+   * Display a listing of the resource.
+   *
+   * @param {object} req
+   * @param {object} res
+   */
+  loadAll(req, res) {
+  }
+
+  /**
+   * Load a resource by its id.
+   *
+   * @param {object} req
+   * @param {object} res
+   */
+  loadById(req, res) {
+  }
+
+  /**
+   * Return the total amount of a specific resource.
+   *
+   * @param {object} req
+   * @param {object} res
+   */
+  count(req, res) {
+  }
+
+  /**
+   * Update the specified resource in storage.
+   *
+   * @param {object} req
+   * @param {object} res
+   */
+  update(req, res) {
+  }
+
+  /**
+   * Remove the specified resource from storage.
+   *
+   * @param {object} req
+   * @param {object} res
+   */
+  destroy(req, res) {
+  }
+}
+
+
--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,26 @@
 import { Router } from 'express';
+import { UserController } from './controllers/user';
 
 /**
  * Contains all API routes for the application.
  */
 const router = Router();
+const userController = new UserController();
 
 router.route('/').get((req, res) => {
   res.json({
     status: 'OK',
   });
 });
+
+router.route('/users/me').post(userController.me);
+router.route('/users/login').post(userController.login);
+router.route('/user/register').post(userController.register);
+
+router.route('/users').get(userController.loadAll);
+router.route('/users/count').get(userController.count);
+router.route('/users/{:userId}').patch(userController.update);
+router.route('/users/{:userId}').post(userController.loadById);
+router.route('/users/{:userId}').delete(userController.destroy);
 
 export default router;
