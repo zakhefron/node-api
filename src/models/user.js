f996e296e5538af8a56ca99ab0b3b77b91b8a22a
Zeeshan Ahmad <ziishaned@gmail.com>
2018-08-31 23:14:33 +0400

    Add user schema

---
+++ src/models/user.js
@@ -0,0 +1,28 @@
+import bcrypt from 'mongoose-bcrypt';
+import mongoose, { Schema } from 'mongoose';
+
+const userSchema = new Schema({
+  email: {
+    type: String,
+    trim: true,
+    unique: true,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    bcrypt: true,
+  },
+  created_at: {
+    type: Date,
+    default: Date.now,
+  },
+  updated_at: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+userSchema.plugin(bcrypt);
+
+export const User = mongoose.model('User', userSchema);
