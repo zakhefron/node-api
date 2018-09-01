25acd2119cdb9f35774a441cd70d25cebb2318d9
Zeeshan Ahmad <ziishaned@gmail.com>
2018-09-01 13:19:29 +0400

    Add express jwt

---
+++ src/helpers/jwt.js
@@ -0,0 +1,19 @@
+class Jwt {
+  /**
+   * Generate a jwt secret token for user.
+   *
+   * @param {*} data
+   */
+  generateToken(data) {
+
+  }
+
+  /**
+   * Decode a provided jwt token.
+   *
+   * @param {string} token
+   */
+  decodeJwtToken(token) {
+
+  }
+}
