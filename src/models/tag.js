e42b780afba32b04fbc6c0568f420c68717bec60
Zeeshan Ahmad <ziishaned@gmail.com>
2018-09-02 12:17:43 +0400

    Add tag model

---
+++ src/models/tag.js
@@ -0,0 +1,34 @@
+import mongoose, { Schema } from 'mongoose';
+
+const tagSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  color: {
+    type: String,
+    required: true,
+  },
+  user_id: {
+    type: String,
+    required: true,
+  },
+  // eslint-disable-next-line camelcase
+  created_at: {
+    type: Date,
+    default: Date.now,
+  },
+  // eslint-disable-next-line camelcase
+  updated_at: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Tag = mongoose.model('Tag', tagSchema);
+
+module.exports = Tag;
